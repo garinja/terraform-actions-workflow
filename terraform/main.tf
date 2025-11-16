02cabecc2610cc0ce224118d7406151defd1253e
garinja <89160170+garinja@users.noreply.github.com>
2025-11-16 13:50:04 -0700

    feat: adding a second pet (#3)
    feat: adding a second pet

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,3 +3,9 @@ resource "random_pet" "this" {
   separator = "-"
   prefix = "${var.environment}"
 }
+
+resource "random_pet" "this2" {
+  length    = 2
+  separator = "-"
+  prefix = "${var.environment}"
+}
